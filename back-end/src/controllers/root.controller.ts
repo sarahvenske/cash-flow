5035508087b897c5e3cb23f52da6df91f43dea19
Sarah Venske Costa <sarahvenske@gmail.com>
2023-08-21 17:53:19 -0300

    feat: most used method logic created

--- back-end/src/controllers/root.controller.ts
+++ back-end/src/controllers/root.controller.ts
@@ -3,13 +3,20 @@ import {
   lowestSpentCategoryService,
   topSpentCategoriesServices,
 } from "../services"
+import { mostUsedMethodService } from "../services/mostUsedMethod.services"
 
 const rootController = async (req: Request, res: Response) => {
   const topSpentCategories = await topSpentCategoriesServices()
 
   const lowestSpentCategory = await lowestSpentCategoryService()
 
-  return res.render("index", { topSpentCategories, lowestSpentCategory })
+  const mostUsedMethod = await mostUsedMethodService()
+
+  return res.render("index", {
+    topSpentCategories,
+    lowestSpentCategory,
+    mostUsedMethod,
+  })
   //   return res.status(200).json(lowestSpentCategory)
 }
 

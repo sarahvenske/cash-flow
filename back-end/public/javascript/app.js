85ce7bbb53d829a1e7342f73832c607bbaef13f4
Sarah Venske Costa <sarahvenske@gmail.com>
2023-08-22 18:03:33 -0300

    feat: render sections logic created

--- back-end/public/javascript/app.js
+++ back-end/public/javascript/app.js
@@ -1,1 +1,31 @@
-console.log("hello")
+const sections = document.querySelectorAll(".slide")
+const next = document.querySelector(".btnNext")
+const previous = document.querySelector(".btnPrevious")
+
+let currentSectionIndex = 0
+
+function activateSlide(number) {
+  sections.forEach((s) => s.classList.add("hidden"))
+  sections[number].classList.remove("hidden")
+}
+activateSlide(currentSectionIndex)
+
+function nextSlide(e) {
+  currentSectionIndex++
+  if (currentSectionIndex == sections.length) {
+    currentSectionIndex = 0
+    activateSlide(currentSectionIndex)
+  }
+  activateSlide(currentSectionIndex)
+}
+
+function previousSlide(e) {
+  currentSectionIndex = currentSectionIndex - 1
+  if (currentSectionIndex < 0) {
+    currentSectionIndex = 0
+    activateSlide(currentSectionIndex)
+  }
+  activateSlide(currentSectionIndex)
+}
+next.addEventListener("click", nextSlide)
+previous.addEventListener("click", previousSlide)
